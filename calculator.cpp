394540cd6f7cfe524fd0af755d02f472821851fe
Vodzinskiy <roma.vodzinskiy@gmail.com>
2022-09-29 22:13:17 +0300

    add patches and modified calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,5 +1,9 @@
 #include "calculator.h"
-
+//created by student 
+//of group IO-04
+//Vodzinskiy Roman
+//some changes 1
+//some changes 2
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
